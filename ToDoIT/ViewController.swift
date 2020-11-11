eba1a74dedd4af16868c4bfd0a874ddd111f113e
Laura Garcia <laudeckeddevs@gmail.com>
2020-11-11 13:45:34 +0100

    realmswift inclusion and WIP - TableView for todos list view

--- ToDoIT/ViewController.swift
+++ ToDoIT/ViewController.swift
@@ -5,15 +5,36 @@
 //  Created by Laura Garcia on 10/11/2020.
 //
 
+import RealmSwift
 import UIKit
 
-class ViewController: UIViewController {
+    //MARK: - ToDoListItemClass
+
+class ToDoListItem: Object {
+    @objc dynamic var item: String = ""
+    @objc dynamic var date: Date = Date()
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+ 
+    //MARK: - Properties
+    
+    @IBOutlet var table: UITableView!
+    private var data = [ToDoListItem]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    //MARK: - TableView
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
 }
 
